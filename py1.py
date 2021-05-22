19ea832c1366c5fbcf8fbb5a7f06dcc65f557c1d
Andreas Papazafeiropoulos <andreaspapaz@gmail.com>
2021-05-21 21:34:31 -0500

    wip: test

--- py1.py
+++ py1.py
@@ -7,6 +7,19 @@ x = [
         [-1.5, 2.7, 3.3, -0.8]
     ]
 
+inputs = [0, 2, -1, 3.3, -2.7, 1.1, 2.2, -100]
+output = []
+
+for i in inputs:
+    if i > 0:
+        output.append(i)
+    elif i <= 0:
+        output.append(0)
+
+
+print(output)
+
+
 class Layer_Dense:
     output = None
     
